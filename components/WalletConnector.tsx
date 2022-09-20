c47f6981b6d7737a07f2a10b865981298e5ac175
kulaneian <kulaneian@outlook.com>
2022-09-20 13:27:43 +0200

    typhon wallet can be enabled

--- components/WalletConnector.tsx
+++ components/WalletConnector.tsx
@@ -1,20 +1,30 @@
 import { Wallet } from '@harmonicpool/cardano-wallet-interface';
 
 export const connectNami = async () => {
-  if (Wallet.has(Wallet.Names.Nami)) {
-    if (!await Wallet.isEnabled(Wallet.Names.Nami)) {
-      Wallet.enable(Wallet.Names.Nami)
-    }
-  }
+  Wallet.enable(Wallet.Names.Nami)
+}
+
+
+export const connectTyphon = async () => {
+  Wallet.enable(Wallet.Names.Typhon)
 }
 
 const WalletConnector = () => {
   return (
-    <button
-      onClick={() => connectNami}
-      className='w-48 mt-8 text-white bg-blue-500 shadow-xl h-14 rounded-xl hover:scale-95'>
-      Connect wallet
-    </button>
+    <div className='flex flex-row gap-10'>
+      <button
+        onClick={() => connectNami()}
+        className='w-48 mt-8 text-white bg-blue-500 shadow-xl h-14 rounded-xl hover:scale-95'>
+        Connect Nami
+      </button>
+
+      <button
+        onClick={() => connectTyphon()}
+        className='w-48 mt-8 text-white bg-blue-500 shadow-xl h-14 rounded-xl hover:scale-95'>
+        Connect Typhon
+      </button>
+    </div>
+
 
   )
 }
